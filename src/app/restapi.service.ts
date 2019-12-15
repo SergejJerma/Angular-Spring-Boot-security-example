daa9b0e2d5ddef73a43ca67834a1b38b17eec86b
Serzas <j_sergej@yahoo.com>
2019-12-15 14:00:50 +0200

    done

---
+++ src/app/restapi.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestapiService {
+
+  constructor(private http:HttpClient) { }
+
+  public login(username:string,password:string){
+    const headers = new HttpHeaders({Authorization:'Basic ' + btoa(username+":"+password)});
+    return this.http.get("http://localhost:8080/", {headers, responseType:'text' as 'json'});
+  }
+
+  public getUsers(){
+    let username = "serjer";
+    let password = "123";
+    const headers = new HttpHeaders({Authorization:'Basic ' + btoa(username+":"+password)});
+    return this.http.get("http://localhost:8080/getUsers", {headers});
+  }
+}
